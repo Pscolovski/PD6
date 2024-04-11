1d84023a0c54db3c1b534d647d3eebd771bda54f
Pscolovski <157747460+Pscolovski@users.noreply.github.com>
2024-04-11 11:23:20 -0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+let funnyNumber = 12345;
+let convertedToString1 = String(funnyNumber);
+console.log(convertedToString1, typeof convertedToString1);
+
+let funnyString = '12345';
+let convertedToNumber2 = Number(funnyString);
+console.log(convertedToNumber2, typeof convertedToNumber2);
+
+let funnyTrue = '1';
+let convertedToBoolean3 = Boolean(funnyTrue);
+console.log(convertedToBoolean3, typeof convertedToBoolean3);
+
+let funnyFalse = '0';
+let convertedToBoolean4 = Boolean(funnyFalse);
+console.log(convertedToBoolean4, typeof convertedToBoolean4);
+
+let isTrue = false;
+let convertedToString5 = String(isTrue);
+console.log(convertedToString5, typeof convertedToString5);
+
+let isFalse = true;
+let convertedToNumber6 = Number(isFalse);
+console.log(convertedToNumber6, typeof convertedToNumber6);
+
+let funnyText = 'JavaScript is dynamic programming language.';
+let convertedToNumber7 = Number(funnyText);
+console.log(convertedToNumber7, typeof convertedToNumber7);
+
+let funnyNull = null;
+let convertedToString8 = String(funnyNull);
+console.log(convertedToString8, typeof convertedToString8);
+
+let funnyUndefined = undefined;
+let convertedToString9 = String(funnyUndefined);
+console.log(convertedToString9, typeof convertedToString9);
+
+let isTrue2 = true;
+let convertedToString10 = String(isTrue2);
+console.log(convertedToString10, typeof convertedToString10);
+
+let isFalse2 = false;
+let convertedToString11 = String(isFalse2);
+console.log(convertedToString11, typeof convertedToString11);
+
+let booleanString = 'true';
+let convertedToNumber12 = Number(booleanString);
+console.log(convertedToNumber12, typeof convertedToNumber12);
+
+let otherBooleanString = 'false';
+let convertedToBoolean13 = Boolean(otherBooleanString);
+console.log(convertedToBoolean13, typeof convertedToBoolean13);
